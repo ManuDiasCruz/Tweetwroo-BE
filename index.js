34761bd27b2e4ed5430dbee0cd9305c3e8e71357
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2022-04-22 19:51:38 -0300

    POST '/signup' implemented

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use(cors())
 let users = []
 let tweets = []
 
+app.post('/sign-up', (request, response) => {
+    const body = request.body
+
+    const user = {
+        username: body.username,
+        avatar: body.avatar
+    }
+    users.push(user)
+    response.send('OK')
+})
 
 app.listen(5000, () =>
     console.log("Servidor no ar em http://localhost/5000")
