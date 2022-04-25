782b6b6ee0763e3eb0f8c90cd74be8af23a02e6e
Manuella Dias Cruz <manuelladcsilvacruz@gmail.com>
2022-04-25 14:52:54 -0300

    Bugs fixed!

--- index.js
+++ index.js
@@ -10,30 +10,46 @@ let tweets = []
 
 app.post('/sign-up', (request, response) => {
     const body = request.body
-
-    const user = {
+    
+    const newUser = {
         username: body.username,
         avatar: body.avatar
     }
-    users.push(user)
-    response.send('OK')
+    users.push(newUser)
+    
+    response.send(users)
 })
 
 app.post('/tweets', (request, response) => {
-    const { msg } = request.body
-    const { user } = request.headers
+    const body = request.body
+    
+    const user = users.find((user) => user.username === body.username)
 
-    const searchUserMatchAvatar = users.find((user) => user.username === user);
-
-    const tweet = {
-        username: msg,
-        avatar: searchUserMatchAvatar.avatar,
-        tweet: msg
+    const newTweet = {
+        username: user.username,
+        avatar: user.avatar,
+        tweet: body.tweet,
     }
-    tweets.push(tweet)
+    tweets.push(newTweet)
+    
     response.send('OK')
 })
 
+app.get('/tweets', (request, response) => {
+    let latestTweets = []
+    let counter = 10
+    if (tweets.length < 10)
+        for (let i = tweets.length-1; i >= 0; i--){
+            latestTweets.push(tweets[i])
+        }
+    else
+        for (let i = tweets.length-1; i >= tweets.length - 10; i--){
+            latestTweets.push(tweets[i])
+        }
+
+    response.send(latestTweets)
+})
+
 app.listen(5000, () =>
-    console.log("Servidor no ar em http://localhost/5000")
+    console.log("Server listening at http://localhost/5000")
 )
